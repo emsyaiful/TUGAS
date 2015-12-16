c2fc97ffd3ca748036cc12d0af0c6d7bbcab2f51
kevin <ashnerandrew@gmail.com>
2015-12-16 14:33:49 +0700

    Buat ClientGame, baru buat main, class beserta attribut dan method basic, akan dikembangkan lebih lagi

---
+++ ClientGame/Client/src/client/Nation.java
@@ -0,0 +1,52 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package client;
+
+/**
+ *
+ * @author asus
+ */
+public class Nation {
+    private int position;
+    private int price;
+    private Player owner;
+    
+    public Nation()
+    {
+        
+    }
+    
+    public Nation(int position,int price)
+    {
+        this.position=position;
+        this.price=price;
+        this.owner=null;
+    }
+    
+    public void buy(Player owner)
+    {
+        this.owner = owner;
+        this.owner.payCash(price);
+    }
+    public void pay(Player payer)
+    {
+        payer.payCash(price);
+        this.owner.addCash(price);
+    }
+    
+    public Player getOwner()
+    {
+        return this.owner;
+    }
+    public int getPosition()
+    {
+        return this.position;
+    }
+    public int getPrice()
+    {
+        return this.price;
+    }
+}
---
+++ ClientGame/Client/src/client/Player.java
@@ -0,0 +1,57 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package client;
+
+/**
+ *
+ * @author asus
+ */
+public class Player {
+    private int cash;
+    private int position;
+    private String name;
+
+    
+    public Player()
+    {
+        
+    }
+    
+    public Player(String name,int cash, int position)
+    {
+        this.name  = name;
+        this.cash  = cash;
+        this.position =position;
+    }
+    
+    public void addCash(int add)
+    {
+        this.cash = this.cash+add;
+    }
+    
+    public void payCash(int pay)
+    {
+        this.cash = this.cash-pay;
+    }
+    
+    public void move(int move)
+    {
+        this.position= position + move;
+    }
+    
+    public int getCash()
+    {
+        return this.cash;
+    }
+    public int getPosition()
+    {
+        return this.position;
+    }
+    public String getName()
+    {
+        return this.name;
+    }
+}
